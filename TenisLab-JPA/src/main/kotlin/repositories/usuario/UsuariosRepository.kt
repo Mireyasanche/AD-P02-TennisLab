c9e464781b562c6d4c63efdedc21cc669e023a0a
Mireya Sánchez Pinzón <mireyasanchz02@gmail.com>
2022-12-07 18:42:21 +0100

    HibernateManager y datos

--- TenisLab-JPA/src/main/kotlin/repositories/usuario/UsuariosRepository.kt
+++ TenisLab-JPA/src/main/kotlin/repositories/usuario/UsuariosRepository.kt
@@ -1,10 +1,22 @@
 package repositories.usuario
 
+import db.HibernateManager
+import db.HibernateManager.manager
 import models.Usuario
+import mu.KotlinLogging
+import javax.persistence.TypedQuery
+
+private val logger = KotlinLogging.logger {}
 
 class UsuariosRepository: IUsuariosRepository {
     override fun findAll(): List<Usuario> {
-        TODO("Not yet implemented")
+        logger.debug { "findAll()" }
+        var usuarios = mutableListOf<Usuario>()
+        HibernateManager.query {
+            val query: TypedQuery<Usuario> = manager.createNamedQuery("Usuario.findAll", Usuario::class.java)
+            usuarios = query.resultList
+        }
+        return usuarios
     }
 
     override fun findById(id: Int): Usuario? {
