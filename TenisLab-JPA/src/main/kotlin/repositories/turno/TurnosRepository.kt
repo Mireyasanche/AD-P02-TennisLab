6af5ac4a3d1d984e519e4d7ef100b0533c2f1de9
Alejandro Sánchez Monzón <alejandrosanchezmonzon1@gmai.com>
2022-12-07 18:41:04 +0100

    Repositorio de turnos.

--- TenisLab-JPA/src/main/kotlin/repositories/turno/TurnosRepository.kt
+++ TenisLab-JPA/src/main/kotlin/repositories/turno/TurnosRepository.kt
@@ -1,21 +1,49 @@
 package repositories.turno
 
 import models.Turno
+import mu.KotlinLogging
+import javax.persistence.TypedQuery
+
+private val logger = KotlinLogging.logger {}
 
 class TurnosRepository: ITurnosRepository {
     override fun findAll(): List<Turno> {
-        TODO("Not yet implemented")
+        logger.debug { "findAll()" }
+        var turnos = mutableListOf<Turno>()
+        HibernateManager.query {
+            val query: TypedQuery<Turno> = manager.createNamedQuery("Turnos.findAll()", Turno::class.java)
+            turnos = query.resultList
+        }
+        return turnos
     }
 
     override fun findById(id: Int): Turno? {
-        TODO("Not yet implemented")
+        logger.debug { "findById($id)" }
+        var turno: Turno? = null
+        HibernateManager.query {
+            turno = manager.find(Turno::class.java, id)
+        }
+        return turno
     }
 
     override fun save(entity: Turno): Turno {
-        TODO("Not yet implemented")
+        logger.debug { "save($entity)" }
+        HibernateManager.transaction {
+            manager.merge(entity)
+        }
+        return entity
     }
 
     override fun delete(entity: Turno): Boolean {
-        TODO("Not yet implemented")
+        var result = false
+        logger.debug { "delete($entity)" }
+        HibernateManager.transaction {
+            val tenista = manager.find(Turno::class.java, entity.uuid)
+            tenista?.let {
+                manager.remove(it)
+                result = true
+            }
+        }
+        return result
     }
 }
