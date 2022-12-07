71d28ece7268ef2f6142ec8e3c0e473e8116d708
Mireya Sánchez Pinzón <mireyasanchz02@gmail.com>
2022-12-07 16:55:55 +0100

    Consultas y estructura del main

--- TennisLab-Exposed/src/main/kotlin/Main.kt
+++ TennisLab-Exposed/src/main/kotlin/Main.kt
@@ -1,18 +1,63 @@
 import config.AppConfig
+import controllers.*
 import db.DataBaseManager
 import db.getUsuariosInit
-import entities.UsuariosDAO
-import repositories.usuario.UsuariosRepository
+import dto.*
+import models.TipoEstado
+import services.StorageJSON
 
 fun main(args: Array<String>) {
     initDataBase()
-    val repo = UsuariosRepository(UsuariosDAO)
+    val maquinasControllers = MaquinasController()
+    val pedidosControllers = PedidosController()
+    val productosController = ProductosController()
+    val tareasController = TareasController()
+    val turnosController = TurnosController()
+    val usuariosController = UsuariosController()
+    val pedidoJSON = StorageJSON<PedidoDTO>()
+    val productoJSON = StorageJSON<ProductoDTO>()
+    val turnoJSON = StorageJSON<TurnoDTO>()
 
     getUsuariosInit().forEach { usuario ->
-        repo.save(usuario)
+        usuariosController.saveUsuario(usuario)
     }
 
-    repo.findAll()
+    usuariosController.getUsuarios().forEach {  usuario ->
+        println(usuario)
+    }
+
+    //Información completa en JSON de un pedido.
+    val pedido = pedidosControllers.getPedido(1).toDTO()
+    pedidoJSON.write("Informacion_completa_pedido", listOf(pedido))
+
+    //Listado de pedidos pendientes en JSON.
+    val pedidosPendientes = pedidosControllers
+        .getPedidos()
+        .filter { it.estado == TipoEstado.EN_PROCESO}
+        .map { it.toDTO()}
+    pedidoJSON.write("listado_pedidos_pendientes", pedidosPendientes)
+
+    //Listado de pedidos completados en JSON.
+    val pedidosCompletados = pedidosControllers
+        .getPedidos()
+        .stream()
+        .filter { it.estado == TipoEstado.TERMINADO}
+        .map { it.toDTO()}.toList()
+    pedidoJSON.write("listado_pedidos_completados", pedidosCompletados)
+
+    //Listado de productos y servicios que ofrecemos en JSON.
+    val productos = productosController
+        .getProductos()
+        .map { it.toDTO() }
+    productoJSON.write("productos_ofrecidos", productos)
+
+    //Listado de asignaciones para los encordadores por fecha en JSON.
+    val asignaciones = turnosController
+        .getTurnos()
+        .sortedBy { it.comienzo }
+        .map { it.toDTO()}
+    turnoJSON.write("listado_asignaciones_encordadores_por_fecha", asignaciones)
+
 }
 
 fun initDataBase() {
